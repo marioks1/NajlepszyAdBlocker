64b9a6e705c9857ba485fd72547443aa7ab9309b
Mariusz Dziuba <mardziuba@gmail.com>
2023-04-22 20:08:15 +0200

    commit

---
+++ ADB/ContentBlockerRequestHandler.swift
@@ -0,0 +1,22 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  ADB
+//
+//  Created by Mariusz Dziuba on 26/03/2023.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
